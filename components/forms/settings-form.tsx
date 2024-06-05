b7469b875713eaa69d5cd192da3abf714d2cc2fe
Rayan Kazi <rayankazi7515@gmail.com>
2024-06-05 11:56:38 -0700

    finished settings form ui

--- components/forms/settings-form.tsx
+++ components/forms/settings-form.tsx
@@ -1,3 +1,7 @@
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
 import { Button } from "~/components/ui/button"
 import {
   Card,
@@ -7,9 +11,29 @@ import {
   CardHeader,
   CardTitle
 } from "~/components/ui/card"
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage
+} from "~/components/ui/form"
 import { Input } from "~/components/ui/input"
 
+const formSchema = z.object({
+  url: z.string().url().min(2)
+})
+
 const SettingsForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      url: ""
+    }
+  })
+
+  const onSubmit = ({ url }: z.infer<typeof formSchema>) => {}
+
   return (
     <main className="flex min-h-[calc(100vh_-_theme(spacing.16))] flex-1 flex-col gap-4 bg-muted/40 p-4 md:gap-8 md:p-10">
       <div>
@@ -17,24 +41,39 @@ const SettingsForm = () => {
           Extra Settings
         </h1>
       </div>
+
       <div className="max-w-4xl items-start">
         <div className="grid gap-6">
-          <Card x-chunk="dashboard-04-chunk-1">
-            <CardHeader>
-              <CardTitle>Custom GIF</CardTitle>
-              <CardDescription>
-                See a custom GIF when you go on a forbidden page.
-              </CardDescription>
-            </CardHeader>
-            <CardContent>
-              <form>
-                <Input placeholder="URL" />
-              </form>
-            </CardContent>
-            <CardFooter className="border-t px-6 py-4">
-              <Button>Save</Button>
-            </CardFooter>
-          </Card>
+          <Form {...form}>
+            <Card>
+              <CardHeader>
+                <CardTitle>Custom GIF</CardTitle>
+                <CardDescription>
+                  See a custom GIF when you go on a forbidden page.
+                </CardDescription>
+              </CardHeader>
+              <CardContent>
+                <form onSubmit={form.handleSubmit(onSubmit)}>
+                  <FormField
+                    control={form.control}
+                    name="url"
+                    render={({ field }) => (
+                      <FormItem>
+                        <FormControl>
+                          <Input placeholder="URL" {...field} />
+                        </FormControl>
+                        <FormMessage />
+                      </FormItem>
+                    )}
+                  />
+                </form>
+              </CardContent>
+
+              <CardFooter className="border-t px-6 py-4">
+                <Button>Save</Button>
+              </CardFooter>
+            </Card>
+          </Form>
         </div>
       </div>
     </main>
